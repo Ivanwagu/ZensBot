fececbe8d9577bcd59a3dedfdb8f8ea366328aa6
Ivanwagu <108420844+Ivanwagu@users.noreply.github.com>
2022-10-21 05:29:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,11 +16,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['62895604670507']
-global.ownernomer = "62895604670507"
-global.premium = ['62895604670507','6281328144300']
-global.packname = 'Zenss'
-global.author = 'Cuyy'
+global.owner = ['6285876830674']
+global.ownernomer = "6285876830674"
+global.premium = ['6285876830674','6285876830674']
+global.packname = 'sans'
+global.author = 'sans'
 global.sessionName = 'zenss'
 global.prefa = ['','!','.','#','&']
 global.sp = ''
